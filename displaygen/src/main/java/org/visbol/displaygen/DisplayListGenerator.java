c7dd9712eaf42623c3c98ae518c7c5be5e98eb45
James Alastair McLaughlin <j.a.mclaughlin@ncl.ac.uk>
2015-10-07 16:57:07 +0100

    Add support for collections

--- displaygen/src/main/java/org/visbol/displaygen/DisplayListGenerator.java
+++ displaygen/src/main/java/org/visbol/displaygen/DisplayListGenerator.java
@@ -31,134 +31,159 @@ public class DisplayListGenerator
 
         for(ComponentDefinition componentDefinition : document.getComponentDefinitions())
         {
-            org.visbol.displaylist.Segment segment = new org.visbol.displaylist.Segment();
+        	addComponentDefinition(componentDefinition); 
+        }
+        
+        for(Collection collection : document.getCollections())
+        {
+        	addCollection(collection);
+        }
+    }
+    
+    private void addCollection(Collection collection)
+    {
+    	for(TopLevel topLevel : collection.getMembers())
+    	{
+    		if(topLevel instanceof ComponentDefinition)
+    		{
+    			addComponentDefinition((ComponentDefinition) topLevel);
+    		}
+    		else if(topLevel instanceof Collection)
+    		{
+    			addCollection((Collection) topLevel);
+    		}
+    	}
+    }
+    
+    private void addComponentDefinition(ComponentDefinition componentDefinition)
+    {
+        org.visbol.displaylist.Segment segment = new org.visbol.displaylist.Segment();
 
-            segment.name = componentDefinition.getName();
+        segment.name = componentDefinition.getName();
 
-            if(segment.name == null)
-                segment.name = componentDefinition.getDisplayId();
+        if(segment.name == null)
+            segment.name = componentDefinition.getDisplayId();
 
-            for(SequenceAnnotation annotation : componentDefinition.getSequenceAnnotations())
+        for(SequenceAnnotation annotation : componentDefinition.getSequenceAnnotations())
+        {
+            Component component = annotation.getComponent();
+
+            String glyphCode = null;
+
+            String glyphName = annotation.getName();
+
+            if(glyphName == null)
+                glyphName = component.getName();
+
+            String glyphID = annotation.getDisplayId();
+
+            if(glyphID == null || glyphID.length() == 0)
+                glyphID = component.getDisplayId();
+
+            if(glyphName == null)
+                glyphName = component.getDefinition().getName();
+
+            if(glyphName == null)
+                glyphName = glyphID;
+
+            String glyphStrand = "positive";
+
+            Set<Location> locations = annotation.getLocations();
+
+            int start = Integer.MAX_VALUE;
+            int end = Integer.MIN_VALUE;
+
+            for(Location location : locations)
             {
-                Component component = annotation.getComponent();
+                if(location.getOrientation() == OrientationType.REVERSECOMPLEMENT)
+                    glyphStrand = "negative";
 
-                String glyphCode = null;
-
-                String glyphName = annotation.getName();
-
-                if(glyphName == null)
-                    glyphName = component.getName();
-
-                String glyphID = annotation.getDisplayId();
-
-                if(glyphID == null || glyphID.length() == 0)
-                    glyphID = component.getDisplayId();
-
-                if(glyphName == null)
-                    glyphName = component.getDefinition().getName();
-
-                if(glyphName == null)
-                    glyphName = glyphID;
-
-                String glyphStrand = "positive";
-
-                Set<Location> locations = annotation.getLocations();
-
-                int start = Integer.MAX_VALUE;
-                int end = Integer.MIN_VALUE;
-
-                for(Location location : locations)
+                if(location instanceof Range)
                 {
-                    if(location.getOrientation() == OrientationType.REVERSECOMPLEMENT)
-                        glyphStrand = "negative";
-
-                    if(location instanceof Range)
-                    {
-                        start = Math.min(start, ((Range) location).getStart());
-                        end = Math.max(end, ((Range) location).getEnd());
-                    }
+                    start = Math.min(start, ((Range) location).getStart());
+                    end = Math.max(end, ((Range) location).getEnd());
                 }
-
-                ComponentDefinition annotationComponentDefinition = component.getDefinition();
-
-                if(annotationComponentDefinition == null)
-                    continue;
-
-                for(URI uri : annotationComponentDefinition.getRoles())
-                {
-                    String path = uri.getPath();
-
-                    /* Look for SO of the form SO_*
-                     */
-                    String[] pathSO = path.split("SO_");
-
-                    /* If not found, look for SO of the form SO:*
-                     */
-                    if(pathSO.length != 2)
-                        pathSO = path.split("SO:");
-
-                    /* If found, map to a glyph
-                     */
-                    if(pathSO.length == 2)
-                    {
-                        String soNum = pathSO[1];
-
-                        try
-                        {
-                            HttpURLConnection connection = (HttpURLConnection)
-                                    new URL("http://dasbol.org/so/SO:" + soNum).openConnection();
-
-                            BufferedReader reader = new BufferedReader(
-                                    new InputStreamReader(connection.getInputStream()));
-
-                            StringBuilder builder = new StringBuilder();
-
-                            String line;
-
-                            while ((line = reader.readLine()) != null)
-                                builder.append(line + "\n");
-
-                            SOMapping mapping = new Gson().fromJson(builder.toString(), SOMapping.class);
-
-                            glyphCode = mapping.glyph;
-                        }
-                        catch(Exception e)
-                        {
-                            /* glyphCode will be null */
-                        }
-
-                    }
-                }
-
-                if(glyphCode == null)
-                    glyphCode = "user-defined";
-
-                int idNumber = glyphTypeIDs.getOrDefault(glyphCode, 0) + 1;
-
-                glyphTypeIDs.put(glyphCode, idNumber);
-
-                org.visbol.displaylist.Glyph glyph = new org.visbol.displaylist.Glyph(glyphCode,
-                        glyphName,
-                        glyphCode + "-" + idNumber,
-                        glyphStrand,
-                        start,
-                        end);
-
-                segment.sequence.add(glyph);
             }
 
-            if(segment.sequence.size() > 0)
-            {
-                segment.sequence.sort(new Comparator<Glyph>()
-                {
-                    public int compare(Glyph glyphA, Glyph glyphB)
-                    {
-                        return glyphA.start - glyphB.start;
-                    }
-                });
+            ComponentDefinition annotationComponentDefinition = component.getDefinition();
 
-                displayList.components.get(0).segments.add(segment);
+            if(annotationComponentDefinition == null)
+                continue;
+
+            for(URI uri : annotationComponentDefinition.getRoles())
+            {
+                String path = uri.getPath();
+
+                /* Look for SO of the form SO_*
+                 */
+                String[] pathSO = path.split("SO_");
+
+                /* If not found, look for SO of the form SO:*
+                 */
+                if(pathSO.length != 2)
+                    pathSO = path.split("SO:");
+
+                /* If found, map to a glyph
+                 */
+                if(pathSO.length == 2)
+                {
+                    String soNum = pathSO[1];
+
+                    try
+                    {
+                        HttpURLConnection connection = (HttpURLConnection)
+                                new URL("http://dasbol.org/so/SO:" + soNum).openConnection();
+
+                        BufferedReader reader = new BufferedReader(
+                                new InputStreamReader(connection.getInputStream()));
+
+                        StringBuilder builder = new StringBuilder();
+
+                        String line;
+
+                        while ((line = reader.readLine()) != null)
+                            builder.append(line + "\n");
+
+                        SOMapping mapping = new Gson().fromJson(builder.toString(), SOMapping.class);
+
+                        glyphCode = mapping.glyph;
+                    }
+                    catch(Exception e)
+                    {
+                        /* glyphCode will be null */
+                    }
+
+                }
             }
+
+            if(glyphCode == null)
+                glyphCode = "user-defined";
+
+            int idNumber = glyphTypeIDs.getOrDefault(glyphCode, 0) + 1;
+
+            glyphTypeIDs.put(glyphCode, idNumber);
+
+            org.visbol.displaylist.Glyph glyph = new org.visbol.displaylist.Glyph(glyphCode,
+                    glyphName,
+                    glyphCode + "-" + idNumber,
+                    glyphStrand,
+                    start,
+                    end);
+
+            segment.sequence.add(glyph);
+        }
+
+        if(segment.sequence.size() > 0)
+        {
+            segment.sequence.sort(new Comparator<Glyph>()
+            {
+                public int compare(Glyph glyphA, Glyph glyphB)
+                {
+                    return glyphA.start - glyphB.start;
+                }
+            });
+
+            displayList.components.get(0).segments.add(segment);
         }
     }
 
